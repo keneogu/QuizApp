0c1507018cdcf98b588b196f448cdaf9b39c515a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-05-25 11:53:20 +0100

    Add function to load quiz and Add event listener to submit button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const quizContain = [
+const quizData = [
   {
     question: "How old is Kene",
     a: "10",
@@ -39,3 +39,36 @@ const quizContain = [
     corret: "d",
   },
 ];
+
+const answerEls = document.querySelectorAll(".answer");
+const questionE1 = document.getElementById("question");
+
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  questionE1.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener('click', () => {
+  currentQuiz++;
+  
+  if(currentQuiz < quizData.length) {
+    loadQuiz();
+  }else {
+    alert("You finished")
+  }
+})
